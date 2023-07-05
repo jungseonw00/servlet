0b5473c0943792b746bd7324bf66e230e944014c
정선우 <laborlawseon@gmail.com>
2023-07-05 22:58:59 +0900

    html 추가

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,10 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+// 내 패키지를 포함해서 하위 패키지를 스캔한다.
+@ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
 
